df744bab2d200037b345c1cde8aeb215d2b533f1
AngelinFaithSheeba <angelinfaithsheeba.p@syncfusion.com>
2023-05-22 20:52:52 +0530

    table added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { PivotViewModule } from '@syncfusion/ej2-angular-pivotview';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, PivotViewModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
